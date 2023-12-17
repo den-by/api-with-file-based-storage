e937f1516b5862600681f371aa9d922f3c1294c4
Dzianis Buyakou <denis.da.by@gmail.com>
2023-12-17 20:48:01 +0100

    test: Add benchmark tests for DataBaseRepository.set method

--- benchmarks/benchmark-test.ts
+++ benchmarks/benchmark-test.ts
@@ -1,14 +1,32 @@
 import Benchmark from 'benchmark';
+import { DataBaseRepository } from "../src/data-base.repository";
+
+const CACHE_DIR = '../.cache';
+
+function getRandomFileName() {
+  return Math.random().toString(36).substring(7);
+}
+
+const options = { 'async': true, initCount: 50, maxTime:  1, minSamples: 5, minTime: -Infinity };
+
+let countFiles = 0;
+let dataBase : DataBaseRepository;
 
 let suite = new Benchmark.Suite();
-
-suite.add('exampleTest', async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-})
-  .on('cycle', (event: Benchmark.Event) => {
+suite.add('DataBaseRepository.set', async () => {
+  countFiles++;
+  const fileName = getRandomFileName();
+  await dataBase.set(fileName, 'test', 0);
+},options)
+  .on('cycle', async (event: Benchmark.Event) => {
     console.log(String(event.target));
   })
-  .on('complete', function(this: Benchmark.Suite) {
+  .on('complete', async function(this: Benchmark.Suite) {
     console.log('Fastest is ' + this.filter('fastest').map('name'));
+    console.log('countFiles:', countFiles);
+    await dataBase.clear();
   })
-  .run({ 'async': true });
+  .on('start', async () => {
+    dataBase = await DataBaseRepository.create({ rootFolderPath: CACHE_DIR, dataFolderName: 'data' });
+  })
+  .run(options);
