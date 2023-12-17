504005a01b11255a589198278f83da6cdbb5d8a3
Dzianis Buyakou <denis.da.by@gmail.com>
2023-12-17 09:12:18 +0100

    feat: Add utils for file operations with retry logic

---
+++ src/utils/file-operations.ts
@@ -0,0 +1,20 @@
+import { retryfy } from "./retryfy";
+import fs from "fs";
+
+export const writeFilePromise = retryfy(fs.promises.writeFile);
+export const readFilePromise = retryfy(fs.promises.readFile);
+export const unlinkPromise = retryfy(fs.promises.unlink);
+export const readdirPromise = retryfy(fs.promises.readdir);
+export const mkdirPromise = retryfy(fs.promises.mkdir);
+
+export const isDirectoryExists = async (dir: string): Promise<boolean>  => {
+  return Boolean(await fs.promises.stat(dir).catch(() => false));
+}
+export const isFileExists = async (filePath: string): Promise<boolean> => {
+  return Boolean(await fs.promises.stat(filePath).catch(() => false));
+}
+export const rmDir = async (dir: string) => {
+  await fs.promises.rm(dir, { recursive: true, force: true, maxRetries: 3, retryDelay: 100 });
+}
+
+
