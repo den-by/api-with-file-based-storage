dd0ee2e7318e670c029fb916437decbc9f144c2c
Dzianis Buyakou <denis.da.by@gmail.com>
2023-12-18 06:42:53 +0100

    refactor: server index file, type definitions and resolvers for better file structure and code readability.

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,18 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 
-import { DataBaseRepository } from './data-base.repository';
+import { defineResolvers } from './resolvers';
+import { typeDefs } from './type-defs';
 
-const typeDefs = gql`
-  scalar AnyType
-
-  type Query {
-    get(key: String!): String
-  }
-  type Mutation {
-    set(key: String!, value: String!, ttl: Int!): String
-    delete(key: String!): String
-    clear: String
-  }
-`;
-
-const CACHE_DIR = '../.cache';
-
-(async () => {
-  const dataBase = await DataBaseRepository.create({ rootFolderPath: CACHE_DIR, dataFolderName: 'data' });
-  const resolvers = {
-    Query: {
-      get: async (_: any, { key }: { key: string }) => {
-        return await dataBase.get(key);
-      },
-    },
-    Mutation: {
-      set: async (_: any, { key, value, ttl }: { key: string; value: string; ttl: number }) => {
-        await dataBase.set(key, value, ttl);
-        return 'ok';
-      },
-      delete: async (_: any, { key }: { key: string }) => {
-        await dataBase.delete(key);
-        return 'ok';
-      },
-      clear: async () => {
-        await dataBase.clear();
-        return 'ok';
-      },
-    },
-  };
+async function init() {
+  const resolvers = await defineResolvers();
   const server = new ApolloServer({ typeDefs, resolvers });
 
-  server.listen().then(({ url }) => {
+  try {
+    const { url } = await server.listen();
     console.log(`🚀  Server ready at ${url}`);
-  });
-})();
+  } catch (error) {
+    console.error(`Error starting server: ${error}`);
+  }
+}
+
+init();
